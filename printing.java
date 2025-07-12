a5b4f7cc9457e630ff3b2fc40f1cc7b745773b5b
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-12 16:25:16 +0530

    refactor printing class to improve output formatting

--- printing.java
+++ printing.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+
 public class printing {
     public static void main(String[] args) {
-        String ans = 
+        String ans = new Integer(50) +""+ new ArrayList<>();
+        System.out.println(ans);
+        
+        System.out.println(new Integer(50) +""+ new ArrayList<>());
+
+        System.out.println(new Integer(50) +""+ new Integer(50));
     }
 }
